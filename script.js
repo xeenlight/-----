7eaac3fd826bd033c7893e797070920fcfde352d
xeenlight <vovabvd@gmail.com>
2024-09-07 18:46:30 +0500

    kinopoisk

---
+++ script.js
@@ -0,0 +1,12 @@
+// let name1 = prompt ('Введите имя');
+// let age = 77;
+// let city = 'Tashkent';
+
+
+// if(name1 == 'Yunus'){ё
+
+//     alert('Привет')
+// }
+//  else{
+//     alert('Пока')
+// }
